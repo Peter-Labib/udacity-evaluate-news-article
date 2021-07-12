0019c63c549b5aceafd23af11c8130a8771f96e1
Peter Labib <peter.adel.labib@gmail.com>
2021-07-13 01:50:11 +0200

    add testing

---
+++ __test__/testSubmitHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from '../src/client/js/submitHandler'
+
+describe('Testing the submit functionality', () => {
+  test('Testing the handleSubmit() function', () => {
+    expect(handleSubmit).toBeDefined()
+  })
+})
